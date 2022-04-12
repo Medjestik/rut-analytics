21193ebd5ecdae5d72f10d08b8a140e448c3b75b
Kostyulin Ivan <ivan-kostyulin@yandex.ru>
2022-04-12 15:36:35 +0300

    feat: builings

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -39,17 +39,17 @@ function Main() {
 
   return (
     <div className="main container">
-      <h1 className="main__title">Аналитическая платформа РУТ&nbsp;(МИИТ)</h1>
+      <h1 className="main__title">Анализ имущественного комплекса РУТ&nbsp;(МИИТ)</h1>
       <ul className="main__list">
         <li className="main__item">
-          <div className="main__item-img main__item-img_type_indicator"></div>
-          <h3 className="main__item-name">Ключевые показатели</h3>
+          <div className="main__item-img main__item-img_type_institute"></div>
+          <h3 className="main__item-name">По институтам (структурные подразделения 1&nbsp;уровня)</h3>
           <a className="main__item-link main__item-link_type_block" href="/" target="_blank" rel="noreferrer">Просмотреть</a>
         </li>
         <li className="main__item">
-          <div className="main__item-img main__item-img_type_finance"></div>
-          <h3 className="main__item-name">Финансовое результаты</h3>
-          <button className="main__item-link" onClick={isShowFinance}>Просмотреть</button>
+          <div className="main__item-img main__item-img_type_building"></div>
+          <h3 className="main__item-name">По объектам<br></br>(зданиям)</h3>
+          <a className="main__item-link main__item-link_type_block" href="/" target="_blank" rel="noreferrer">Просмотреть</a>
           <div className={`main__item-menu ${showFinance ? "main__item-menu_type_show" : "main__item-menu_type_hide"}`}>
             <button className="main__close-menu" type="button" onClick={isHideAll}></button>
               <a className="main__menu-item main__menu-item_type_block" href="http://83.220.46.36/viewer/public?dashboardGuid=87c5fa2978bb4def8efc0c024bd85bf9" target="_blank" rel="noreferrer">
@@ -67,9 +67,9 @@ function Main() {
           </div>
         </li>
         <li className="main__item">
-          <div className="main__item-img main__item-img_type_production"></div>
-          <h3 className="main__item-name">Производственная статистика</h3>
-          <button className="main__item-link" onClick={isShowProduction}>Просмотреть</button>
+          <div className="main__item-img main__item-img_type_indicator"></div>
+          <h3 className="main__item-name">Итоговые<br></br>показатели</h3>
+          <a className="main__item-link main__item-link_type_block" href="/" target="_blank" rel="noreferrer">Просмотреть</a>
           <div className={`main__item-menu ${showProduction ? "main__item-menu_type_show" : "main__item-menu_type_hide"}`}>
             <button className="main__close-menu" type="button" onClick={isHideAll}></button>
               <a className="main__menu-item" href="http://83.220.46.36/viewer/public?dashboardGuid=c56630f9f8e946e18eadf6a2767812a7&showNav=true" target="_blank" rel="noreferrer">
@@ -87,9 +87,9 @@ function Main() {
           </div>
         </li>
         <li className="main__item">
-          <div className="main__item-img main__item-img_type_personal"></div>
-          <h3 className="main__item-name">Персонал</h3>
-          <button className="main__item-link" onClick={isShowPersonal}>Просмотреть</button>
+          <div className="main__item-img main__item-img_type_structure"></div>
+          <h3 className="main__item-name">Структурные подразделения 2&nbsp;уровня</h3>
+          <a className="main__item-link main__item-link_type_block" href="/" target="_blank" rel="noreferrer">Просмотреть</a>
           <div className={`main__item-menu ${showPersonal ? "main__item-menu_type_show" : "main__item-menu_type_hide"}`}>
             <button className="main__close-menu" type="button" onClick={isHideAll}></button>
               <a className="main__menu-item" href="http://83.220.46.36/viewer/public?dashboardGuid=0eef5c3b42a140a98bfe02ce9cd44bba" target="_blank" rel="noreferrer">
@@ -107,9 +107,9 @@ function Main() {
           </div>
         </li>
         <li className="main__item">
-          <div className="main__item-img main__item-img_type_property"></div>
-          <h3 className="main__item-name">Имущество</h3>
-          <a className="main__item-link" href="http://83.220.46.36/viewer/public?dashboardGuid=5be9ca95ef7e418b89e2aad92d326a02&showNav=true" target="_blank" rel="noreferrer">Просмотреть</a>
+          <div className="main__item-img main__item-img_type_calculation"></div>
+          <h3 className="main__item-name">Расчет рыночной стоимости объектов</h3>
+          <a className="main__item-link main__item-link_type_block" href="/" target="_blank" rel="noreferrer">Просмотреть</a>
         </li>
       </ul>
     </div>
